bf56c83af4234a47cc0045cf79999207d977818d
hilal555 <muhamadhilal04@gmail.com>
2025-12-14 03:49:35 +0700

    hapus button detail siswa yg tidak digunakan

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,3 @@
-// vite.config.js (DIPERBARUI)
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
@@ -14,11 +13,7 @@ export default defineConfig({
     ],
     resolve: {
         alias: {
-            // ALIAS UTAMA (Sudah ada)
             '@': path.resolve(__dirname, 'resources/js'),
-            
-            // ALIAS BARU: Menambahkan alias khusus untuk folder assets
-            // Ini akan menyelesaikan masalah jika Anda ingin menggunakan import heroImage from "@assets/hero-image.jpg"
             '@assets': path.resolve(__dirname, 'resources/js/assets'),
         },
     },
